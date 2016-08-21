31c6e6caee2ecdc5e4593681cb9d872ad8b2ca6e
railsbeginner <bartosz.dyja@gmail.com>
2016-08-21 18:06:48 +0200

    Add image to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821155102) do
+ActiveRecord::Schema.define(version: 20160821160629) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.string   "image"
   end
 
 end
